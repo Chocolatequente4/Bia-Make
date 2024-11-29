528e0dd76f27ba48770b9f28abae23945987bdec
Yuri²² <00001120695089sp@al.educacao.sp.gov.br>
2024-11-29 12:48:11 -0300

    Initial commit

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Maquiagem',
+    'Qual é a função do primer na maquiagem?',
+    'O primer prepara a pele, ajuda a suavizar a textura e aumenta a durabilidade da maquiagem.'
+)
+
+criaCartao(
+    'Maquiagem',
+    'Como escolher o tom certo de base?',
+    'Teste a cor na linha da mandíbula e escolha a mais próxima do tom da pele.'
+)
+
+criaCartao(
+    'Maquiagem',
+    'Qual a diferença entre pó translúcido e compacto?',
+    'O pó translúcido fixa a maquiagem sem cor, e o compacto controla a oleosidade e oferece mais cobertura.'
+)
+
+criaCartao(
+    'Maquiagem',
+    'Como fazer a maquiagem dos olhos durar mais tempo?',
+    'Para fazer a maquiagem dos olhos durar mais, use um primer específico para os olhos antes de aplicar as sombras e finalize com um spray fixador para garantir mais duração.'
+)
+
+criaCartao(
+    'Maquiagem',
+    'Qual a diferença entre maquiagem matte e iluminada?',
+    'A maquiagem matte proporciona um acabamento mais opaco, ideal para peles oleosas, enquanto a maquiagem iluminada destaca áreas do rosto com brilho, criando um efeito mais radiante e fresco.'
+)
